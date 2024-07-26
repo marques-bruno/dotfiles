e091b20d4d7207377562b7408223c3ac0de5060e
marques-bruno <bmarques68@gmail.com>
2024-07-26 10:22:38 +0200

    Remove all icons, themes... Add nvim / tmux config

---
+++ .zsh/zsh/config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# VI bindings in zsh, sweet...
+bindkey -v
+
+# my own custom scripts and binaries are usually in .bin or .bin/scripts
+export PATH=.bin/scripts:/home/bruno/.bin:$PATH
+
+# ruby gems
+export PATH=/home/bruno/.local/share/gem/ruby/3.0.0/bin:$PATH
+
+# ccache for faster builds
+export PATH=/usr/lib/ccache/bin/:$PATH
---
+++ .zsh/zsh/cpp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#export CMAKE_GENERATOR="Ninja Multi-Config"
+#export CMAKE_DEFAULT_BUILD_TYPE=RelWithDebInfo
